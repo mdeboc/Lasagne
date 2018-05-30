6d5c6d3e1cea09e01315222261a00f136d4d60ec
mdeboc <mdeboc@zoho.com>
2018-05-30 14:50:29 +0200

    [F #21] add nadam() method in uptades by copying adam()

--- lasagne/updates.py
+++ lasagne/updates.py
@@ -631,6 +631,72 @@ def adam(loss_or_grads, params, learning_rate=0.001, beta1=0.9,
     return updates
 
 
+def nadam(loss_or_grads, params, learning_rate=0.001, beta1=0.9,
+         beta2=0.999, epsilon=1e-8):
+    """Nadam updates
+
+    Nadam updates implemented as in [1]_.
+
+    Parameters
+    ----------
+    loss_or_grads : symbolic expression or list of expressions
+        A scalar loss expression, or a list of gradient expressions
+    params : list of shared variables
+        The variables to generate update expressions for
+    learning_rate : float or symbolic scalar
+        Learning rate
+    beta1 : float or symbolic scalar
+        Exponential decay rate for the first moment estimates.
+    beta2 : float or symbolic scalar
+        Exponential decay rate for the second moment estimates.
+    epsilon : float or symbolic scalar
+        Constant for numerical stability.
+
+    Returns
+    -------
+    OrderedDict
+        A dictionary mapping each parameter to its update expression
+
+    Notes
+    -----
+    The paper [1]_ improves the Adam optimization algorithm with
+    an accelerated gradient a la Nesterov.
+
+    References
+    ----------
+    .. [1] Timothy Dozat (2015):
+           Incorporating Nesterov Momentum into Adam
+           http://cs229.stanford.edu/proj2015/054_report.pdf
+    """
+    all_grads = get_or_compute_grads(loss_or_grads, params)
+    t_prev = theano.shared(utils.floatX(0.))
+    updates = OrderedDict()
+
+    # Using theano constant to prevent upcasting of float32
+    one = T.constant(1)
+
+    t = t_prev + 1
+    a_t = learning_rate*T.sqrt(one-beta2**t)/(one-beta1**t)
+
+    for param, g_t in zip(params, all_grads):
+        value = param.get_value(borrow=True)
+        m_prev = theano.shared(np.zeros(value.shape, dtype=value.dtype),
+                               broadcastable=param.broadcastable)
+        v_prev = theano.shared(np.zeros(value.shape, dtype=value.dtype),
+                               broadcastable=param.broadcastable)
+
+        m_t = beta1*m_prev + (one-beta1)*g_t
+        v_t = beta2*v_prev + (one-beta2)*g_t**2
+        step = a_t*m_t/(T.sqrt(v_t) + epsilon)
+
+        updates[m_prev] = m_t
+        updates[v_prev] = v_t
+        updates[param] = param - step
+
+    updates[t_prev] = t
+    return updates
+
+
 def adamax(loss_or_grads, params, learning_rate=0.002, beta1=0.9,
            beta2=0.999, epsilon=1e-8):
     """Adamax updates
